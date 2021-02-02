9550d3d829779c8277c56df738636b4021687544
wwp <wangweiping@bilibili.com>
2021-02-02 20:09:05 +0800

    提交原始项目漏掉的代码

--- Unity/Assets/Model/Init.cs
+++ Unity/Assets/Model/Init.cs
@@ -28,14 +28,27 @@ namespace ETModel
                 Game.Scene.AddComponent<GlobalConfigComponent>(); //web资源服务器设置组件
                 Game.Scene.AddComponent<ResourcesComponent>(); //资源加载组件
 
+
                 //测试输出正确加载了Config所带的信息
                 ETModel.Game.Scene.GetComponent<ResourcesComponent>().LoadBundle("config.unity3d");
                 Game.Scene.AddComponent<ConfigComponent>();
                 ETModel.Game.Scene.GetComponent<ResourcesComponent>().UnloadBundle("config.unity3d");
                 UnitConfig unitConfig = (UnitConfig)Game.Scene.GetComponent<ConfigComponent>().Get(typeof(UnitConfig), 1001);
                 Log.Debug($"config {JsonHelper.ToJson(unitConfig)}");
+
+
+                //添加指令与网络组件
+                Game.Scene.AddComponent<OpcodeTypeComponent>();
+                Game.Scene.AddComponent<NetOuterComponent>();
+
+                //测试发送给服务端一条文本消息
+                Log.Debug(GlobalConfigComponent.Instance.GlobalProto.Address);
+                Session session = Game.Scene.GetComponent<NetOuterComponent>().Create(GlobalConfigComponent.Instance.GlobalProto.Address);
+                G2C_TestMessage g2CTestMessage = (G2C_TestMessage)await session.Call(new C2G_TestMessage() { Info = "==>>服务端的朋友,你好!收到请回答" });
+
+
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 Log.Error(e);
             }
